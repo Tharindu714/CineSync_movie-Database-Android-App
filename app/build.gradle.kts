1554b3868b43163dbeabe7a9fbbed062350cc56a
Tharindu <110843121+Tharindu714@users.noreply.github.com>
2025-03-22 08:48:47 +0530

    app files

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.deltacodex.memoryme"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.deltacodex.memoryme"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.firebase.firestore)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation (libs.firebase.firestore.v2491)
+    implementation("com.google.firebase:firebase-database:20.2.1")
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+    implementation ("androidx.work:work-runtime:2.8.1")
+}
